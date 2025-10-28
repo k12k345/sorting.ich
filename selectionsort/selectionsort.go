3a4b98c6139fcc1d86b85df080882fd6dd1a2026
kimlia.knopf@heidelberg.com <kimlia.knopf@heidelberg.com>
2025-10-28 17:23:53 +0100

    Implement SelectionSort algorithm in SmallestElementIndex function

--- selectionsort/selectionsort.go
+++ selectionsort/selectionsort.go
@@ -2,11 +2,26 @@ package selectionsort
 
 // SmallestElementIndex gibt den Index des kleinsten Elements im Array arr ab Index start zurück.
 func SmallestElementIndex(arr []int, start int) int {
-	// TODO
+	for i := 0; i < len(arr)-1; i++ {
+
+		minIndex := i
+		for j := i + 1; j < len(arr); j++ {
+			if arr[j] < arr[minIndex] {
+				minIndex = j
+			}
+		}
+
+		if minIndex != i {
+			temp := arr[i]
+			arr[i] = arr[minIndex]
+			arr[minIndex] = temp
+		}
+	}
+
 	return 0
 }
 
 // SelectionSort sortiert die übergebene Liste mittels des Selection-Sort-Algorithmus.
 func SelectionSort(arr []int) {
-	// TODO
+	SmallestElementIndex(arr, 0)
 }
